78b879d84fe5513c129c556e6dc0f1ffbbdf5406
Jayesh <jayeshmalshikare1901@gmail.com>
2024-08-26 15:10:11 +0530

    completed signup page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <header className='bg-slate-200 shadow-md'>
-       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+       <div className='flex justify-between items-center max-w-6xl mx-auto p-1'>
         <Link to='/'>
          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
             <spam classname='text-slate-500'>J</spam>
